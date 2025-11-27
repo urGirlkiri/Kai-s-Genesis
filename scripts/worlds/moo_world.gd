891440ad3ad5cc31817ddbe47d8f270befd5eeb5
Kiri <cultivatingghoul@gmail.com>
2025-11-27 17:16:57 +0200

    fixed clicks  and placements in the void

--- scripts/worlds/moo_world.gd
+++ scripts/worlds/moo_world.gd
@@ -6,6 +6,7 @@ var placing_scene: PackedScene = null
 var is_placing_mode := false
 
 @onready var click_popup_scene = preload("res://scenes/ui/ClickPopup.tscn")
+@onready var tile_map: TileMapLayer = $TileMapLayer
 
 func _ready() -> void:
 	pass
@@ -14,12 +15,17 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			
+			var mouse_pos = get_global_mouse_position()
+			
+			if not is_point_walkable(mouse_pos):
+				print("Clicked into the void!")
+				return
+
 			if is_placing_mode:
-				finalize_placement(get_global_mouse_position())
+				finalize_placement(mouse_pos)
 			else:
-				#todo: further check if click is on placed items
 				Globals.life_force += Globals.CLICK_ENERGY_GAIN
-				spawn_popup(get_global_mouse_position())
+				spawn_popup(mouse_pos)
 
 func start_placement(item_path: String):
 	placing_scene = load(item_path)
@@ -29,19 +35,29 @@ func start_placement(item_path: String):
 func finalize_placement(pos: Vector2):
 	if placing_scene == null: 
 		return
+	
+	if not is_point_walkable(pos):
+		return
 
 	var new_item = placing_scene.instantiate()
-	#todo: check for valid placement area and if possible alignment also
+	#todo: auto align immaterial items like grass 
 	add_child(new_item)
 	new_item.global_position = pos
 	
 	is_placing_mode = false
 	placing_scene = null
 	item_placed_successfully.emit()
+	queue_redraw() # Clear the drawing
 
 func spawn_popup(pos: Vector2):
 	var popup = click_popup_scene.instantiate()
-	
 	add_child(popup)
-
 	popup.setup(pos, 1) 
+
+func is_point_walkable(global_pos: Vector2) -> bool:
+	var local_pos = tile_map.to_local(global_pos)
+	var map_coords = tile_map.local_to_map(local_pos)
+	
+	var source_id = tile_map.get_cell_source_id(map_coords)
+	
+	return source_id != -1
