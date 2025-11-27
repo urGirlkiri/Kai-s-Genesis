641c9cfe628ffb3e5309dc589deb796433ca9973
Kiri <cultivatingghoul@gmail.com>
2025-11-27 23:04:32 +0200

    customized game icon and splash animation

---
+++ scripts/ui/boot_splash.gd
@@ -0,0 +1,19 @@
+extends Control
+
+const MAIN_GAME_SCENE = preload("uid://c2g0pw5amocod")
+
+@onready var progress_bar: TextureProgressBar = $ProgressBar
+
+func _ready() -> void:
+	progress_bar.value = 0
+	start_loading_animation()
+
+func start_loading_animation() -> void:
+	var tween = create_tween()
+	
+	tween.tween_property(progress_bar, "value", 100, 3.0).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)	
+	tween.finished.connect(_on_loading_complete)
+
+func _on_loading_complete() -> void:
+	# Optional: Add a small delay or fade out effect here
+	get_tree().change_scene_to_packed(MAIN_GAME_SCENE)
