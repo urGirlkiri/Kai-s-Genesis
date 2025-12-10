670efd81f01b5619337305df0ec1c0be865bb20f
Kiri <cultivatingghoul@gmail.com>
2025-12-10 20:41:53 +0200

    changed from manual calculation to terrain auto tiling

--- scripts/worlds/moo_world.gd
+++ scripts/worlds/moo_world.gd
@@ -2,13 +2,6 @@ extends World
 
 const EARTH_SOURCE_ID = 0
 
-const neighbor_offsets = {
-	"TOP_LEFT_EARTH_COORD":  Vector2i(1, 0),
-	"BOTTOM_LEFT_EARTH_COORD": Vector2i(1, 2),
-	"TOP_RIGHT_EARTH_COORD": Vector2i(3, 0),
-	"BOTTOM_RIGHT_EARTH_COORD": Vector2i(3, 2),
-}
-
 var land_expander_active := false
 var placement_object: Node = null
 
@@ -32,6 +25,7 @@ func _unhandled_input(event: InputEvent) -> void:
 		var cost = Globals.BUYABLES["Earth"]["cost"]
 		if Globals.life_force < cost:
 			SignalBus.show_message.emit("Not enough Life Force to expand!", "error")
+			land_expander_active = false
 			return
 
 		var click_pos = get_global_mouse_position()
@@ -46,23 +40,15 @@ func _unhandled_input(event: InputEvent) -> void:
 			SignalBus.show_message.emit("Cannot expand here.", "error")
 
 func expand_land(map_coords: Vector2i) -> bool:
-
-	var has_placed_tile := false
-
-	# Check Top Left Neighbhour
-	var current_pos = map_coords + neighbor_offsets['TOP_LEFT_EARTH_COORD']
-	var source_id = tile_map.get_cell_source_id(current_pos)
-
-	if source_id == EARTH_SOURCE_ID:
-		pass
-	else:
-		tile_map.set_cell(map_coords, EARTH_SOURCE_ID, neighbor_offsets['TOP_LEFT_EARTH_COORD'])
-		has_placed_tile = true
+	var source_id = tile_map.get_cell_source_id(map_coords)
 	
-	# Check Top Right Neighbhour
-	
-	return has_placed_tile
+	if source_id != -1: 
+		return false
 
+	tile_map.set_cells_terrain_connect([map_coords], 0, 0, false)
+	
+	return true
+	
 func cancel_placement() -> void:
 	if placement_object:
 		placement_object.queue_free()
