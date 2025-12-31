799f96e8dc36866ad13b8e7960960efb93654e45
Kiri <cultivatingghoul@gmail.com>
2025-12-31 06:53:56 +0200

    added goblins and worked on the third step of the storyline

--- scripts/core/destruction_story.gd
+++ scripts/core/destruction_story.gd
@@ -35,9 +35,9 @@ func advance_story() -> void:
 	index_story(current_line_index)
 	
 	var line_text = STORY[current_line_index]
-	type_text(line_text)
+	type_text(line_text, current_line_index)
 
-func type_text(text_to_show: String) -> void:
+func type_text(text_to_show: String, index : int) -> void:
 	is_typing = true
 	label.text = text_to_show
 	label.visible_ratio = 0.0
@@ -51,23 +51,35 @@ func type_text(text_to_show: String) -> void:
 	is_typing = false
 	
 	await get_tree().create_timer(story_time).timeout
-	advance_story()
+	if current_line_index not in [2]:
+		advance_story()
 
-func index_story(index: int) -> void:	
+func index_story(index: int) -> void:
 	match index:
-		0: 
+		0:
 			anim_sprite.play("sleeping")
-		1: 
+		1:
 			for gob in get_tree().get_nodes_in_group('goblins'):
 				gob.visible = true
 				gob.set_physics_process(true)
 				gob.animated_sprite.play('playing')
 				
-			anim_sprite.play("disturbed") 
-		2: 
-			anim_sprite.play("awaken") 
+			anim_sprite.play("disturbed")
+		2:
+			var goblins = get_tree().get_nodes_in_group('goblins')
+			for gob in goblins:
+				gob.attack(destroyer.global_position)
+			
+			await goblins[0].attack_landed
+			anim_sprite.play("awaken")
+			
+			await anim_sprite.animation_finished
+			advance_story()
+			
 		3:
-			anim_sprite.play("cranky")
+			anim_sprite.play("cranky")		
+			#await anim_sprite.animation_finished
+			#advance_story()
 
 func finish_intro() -> void:
 	pass
--- scripts/entities/goblin.gd
+++ scripts/entities/goblin.gd
@@ -1,8 +1,48 @@
-extends Node2D
+extends RigidBody2D
+
+signal attack_landed 
+
+const SPEED = 400
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+var attackLocation: Vector2
+var isAttacking = false
+var isMoving = false
 
 func _ready() -> void:
 	self.visible = false
 	self.set_physics_process(false)
 	animated_sprite.stop()
+	
+func _process(delta: float) -> void:
+	if isMoving and attackLocation != null and not isAttacking:
+		
+		var direction = (attackLocation - global_position).normalized()
+		
+		if linear_velocity.x < 0: 
+			animated_sprite.flip_h = false
+		else:
+			animated_sprite.flip_h = true
+			
+		linear_velocity = direction * SPEED
+		
+		if animated_sprite.animation != 'move':
+			animated_sprite.play('move')
+
+func _on_target_reached(body: Node) -> void:
+	if isAttacking: return 
+	
+	isAttacking = true
+	isMoving = false 
+	linear_velocity = Vector2.ZERO
+	
+	animated_sprite.play('attack')
+	
+	await animated_sprite.animation_finished
+	attack_landed.emit() 
+
+func attack(pos: Vector2):
+	if isMoving: return
+	isMoving = true
+	attackLocation = pos
